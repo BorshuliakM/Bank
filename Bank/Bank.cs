ac27e0d2399852d17cde5c4a8fa4a4330c6797ef
BorshuliakM <borshuliakm@gmail.com>
2023-11-05 17:17:54 +0200

    Додано статичне поле та метод відвоідно до лаб 2

--- Bank/Bank.cs
+++ Bank/Bank.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Bank;
 
-namespace Bank
+public class Bank // Класс відповідно до варіанту 
 {
-    internal class Bank
+    public string Name { get; set; } //Деякі поля відповідно до лаб 1
+    public string Address { get; set; }
+    private static int id = 0;  // Статичне поле лаб 2
+    private int currentBankId;  // Поточний номер банку
+
+    public Bank(string Name, string Address)
     {
+        this.Name = Name;
+        this.Address = Address;
+        id++; // Збільшення на 1 при створенні нового банку 
+        currentBankId = id; //Присвоєння айди певному банку
     }
+    public int GetBankId() => currentBankId; //Повернути айді банку
+    public static int NumberOfBanks() => id; //Повернути кількість банків
 }
